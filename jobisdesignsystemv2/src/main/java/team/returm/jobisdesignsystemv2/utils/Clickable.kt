9cc8fe0e8379ba6f05947d5f0eb9adca02132a1e
Tmdhoon2 <hks026naver@gmail.com>
2023-12-24 12:50:08 +0900

    feat :: Clickable

---
+++ jobisdesignsystemv2/src/main/java/team/returm/jobisdesignsystemv2/utils/Clickable.kt
@@ -0,0 +1,64 @@
+package team.returm.jobisdesignsystemv2.utils
+
+import android.annotation.SuppressLint
+import android.view.MotionEvent
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.clickable
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.input.pointer.pointerInteropFilter
+
+internal const val DURATION_MILLS = 200
+
+@OptIn(ExperimentalComposeUiApi::class)
+@SuppressLint("ComposableNaming")
+@Composable
+fun Modifier.clickable(
+    enabled: Boolean = true,
+    pressDepth: Float = 0.98f,
+    onPressed: (pressed: Boolean) -> Unit,
+    onClick: () -> Unit,
+): Modifier {
+    var pressed by remember { mutableStateOf(false) }
+    val scale by animateFloatAsState(
+        targetValue = if (pressed) {
+            pressDepth
+        } else {
+            1f
+        },
+        animationSpec = tween(durationMillis = DURATION_MILLS),
+        label = "",
+    )
+
+    return this then Modifier
+        .graphicsLayer {
+            scaleX = scale
+            scaleY = scale
+        }
+        .pointerInteropFilter { event ->
+            when (event.action) {
+                MotionEvent.ACTION_DOWN -> {
+                    pressed = true
+                }
+
+                MotionEvent.ACTION_UP, MotionEvent.ACTION_CANCEL -> {
+                    pressed = false
+                }
+
+                else -> {}
+            }
+            onPressed(pressed)
+            true
+        }
+        .clickable(
+            onClick = onClick,
+            enabled = enabled,
+        )
+}
