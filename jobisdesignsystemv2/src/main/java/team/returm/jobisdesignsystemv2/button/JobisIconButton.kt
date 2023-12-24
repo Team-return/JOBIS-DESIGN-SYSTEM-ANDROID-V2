24b30c9cf109bf7c55d007276b6ca9f5ce19d9eb
Tmdhoon2 <hks026naver@gmail.com>
2023-12-24 12:50:29 +0900

    refactor :: Compose.foundation clickable replace to jobis clickable util

--- jobisdesignsystemv2/src/main/java/team/returm/jobisdesignsystemv2/button/JobisIconButton.kt
+++ jobisdesignsystemv2/src/main/java/team/returm/jobisdesignsystemv2/button/JobisIconButton.kt
@@ -1,15 +1,12 @@
 package team.returm.jobisdesignsystemv2.button
 
-import androidx.compose.animation.core.animateFloatAsState
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.interaction.MutableInteractionSource
-import androidx.compose.foundation.interaction.PressInteraction
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.ripple.rememberRipple
 import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -17,11 +14,12 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.graphics.painter.Painter
-import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.unit.dp
 import team.returm.jobisdesignsystemv2.foundation.JobisColor
+import team.returm.jobisdesignsystemv2.foundation.JobisTheme
+import team.returm.jobisdesignsystemv2.utils.DURATION_MILLS
+import team.returm.jobisdesignsystemv2.utils.clickable
 
 @Composable
 private fun BasicIconButton(
@@ -32,38 +30,27 @@ private fun BasicIconButton(
     enabled: Boolean,
     onClick: () -> Unit,
 ) {
-    val interactionSource = remember { MutableInteractionSource() }
     var pressed by remember { mutableStateOf(false) }
-
-    val scale by animateFloatAsState(
+    val background by animateColorAsState(
         targetValue = if (pressed) {
-            0.9f
+            JobisTheme.colors.inverseSurface
         } else {
-            1f
+            JobisTheme.colors.background
         },
         label = "",
+        animationSpec = tween(durationMillis = DURATION_MILLS),
     )
-
-    LaunchedEffect(Unit) {
-        interactionSource.interactions.collect {
-            pressed = it is PressInteraction.Press
-        }
-    }
 
     Icon(
         modifier = modifier
             .clip(CircleShape)
             .clickable(
                 enabled = enabled,
-                role = Role.Button,
-                interactionSource = interactionSource,
-                indication = rememberRipple(),
                 onClick = onClick,
+                pressDepth = 0.93f,
+                onPressed = { pressed = it },
             )
-            .graphicsLayer {
-                scaleX = scale
-                scaleY = scale
-            }
+            .background(color = background)
             .padding(4.dp),
         painter = painter,
         contentDescription = contentDescription,
--- jobisdesignsystemv2/src/main/java/team/returm/jobisdesignsystemv2/card/JobisCard.kt
+++ jobisdesignsystemv2/src/main/java/team/returm/jobisdesignsystemv2/card/JobisCard.kt
@@ -1,27 +1,49 @@
 package team.returm.jobisdesignsystemv2.card
 
+import androidx.compose.animation.animateColorAsState
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import team.returm.jobisdesignsystemv2.foundation.JobisTheme
+import team.returm.jobisdesignsystemv2.utils.clickable
 
 @Composable
 fun JobisCard(
     modifier: Modifier = Modifier,
     shape: RoundedCornerShape = RoundedCornerShape(12.dp),
     background: Color = JobisTheme.colors.inverseSurface,
+    enabled: Boolean = true,
     onClick: () -> Unit,
     content: @Composable () -> Unit,
 ) {
+    var pressed by remember { mutableStateOf(false) }
+    val backgroundColor by animateColorAsState(
+        targetValue = if (pressed) {
+            JobisTheme.colors.surfaceVariant
+        } else {
+            background
+        },
+        label = "",
+    )
+
     Surface(
-        modifier = modifier.fillMaxWidth(),
+        modifier = modifier
+            .fillMaxWidth()
+            .clickable(
+                onClick = onClick,
+                enabled = enabled,
+                onPressed = { pressed = it },
+            ),
         shape = shape,
-        color = background,
-        onClick = onClick,
+        color = backgroundColor,
     ) {
         content()
     }
