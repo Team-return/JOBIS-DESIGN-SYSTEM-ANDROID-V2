956e31cdf513c058d100c4e9e6fc08e0eb853d37
rladmsdh <a01051355543@gmail.com>
2024-01-30 17:10:19 +0900

    test :: 텍스트 스타일 검증 추가

--- jobisdesignsystemv2/src/androidTest/java/team/returm/jobisdesignsystemv2/JobisTextTest.kt
+++ jobisdesignsystemv2/src/androidTest/java/team/returm/jobisdesignsystemv2/JobisTextTest.kt
@@ -1,9 +1,11 @@
 package team.returm.jobisdesignsystemv2
 
+import androidx.compose.foundation.clickable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createComposeRule
-import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.junit.Rule
 import org.junit.Test
@@ -13,25 +15,28 @@ import team.returm.jobisdesignsystemv2.text.JobisText
 
 const val TEXT_TEST_TAG = "TextTest"
 
+/**
+ * Verifies that text appears on the display and that styles are applied correctly
+ * @see JobisText
+ */
 @RunWith(AndroidJUnit4::class)
 class JobisTextTest {
     @get:Rule
     val composeRule = createComposeRule()
+    private var style = JobisTypography.Body
 
-    /**
-     * Verifies that text appears on the display
-     * @see JobisText
-     */
     @Test
     fun jobisTextTest() {
         composeRule.setContent {
             JobisText(
                 text = TEXT_TEST_TAG,
-                style = JobisTypography.Body,
+                style = style,
+                modifier = Modifier.clickable { style = JobisTypography.Caption }
             )
         }
-        composeRule
-            .onNodeWithText(TEXT_TEST_TAG)
-            .assertIsDisplayed()
+        val test = composeRule.onNodeWithText(TEXT_TEST_TAG).assertIsDisplayed()
+        test.performClick()
+
+        composeRule.runOnIdle { assert(style == JobisTypography.Caption) }
     }
 }
